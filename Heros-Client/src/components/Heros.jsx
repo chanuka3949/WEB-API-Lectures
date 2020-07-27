d19aa5f0c4d4e943c9cfe3176b07ff5bbfff0ff9
chanuka3949 <chanukasds@outlook.com>
2020-07-27 17:54:42 +0530

    Dynamic hero list

--- Heros-Client/src/components/Heros.jsx
+++ Heros-Client/src/components/Heros.jsx
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 import Hero from "./Hero";
 
 class Heros extends Component {
-  state = {};
+  state = {
+    allAvengers: [
+      { id: 1, likeCount: 5 },
+      { id: 2, likeCount: 15 },
+      { id: 3, likeCount: 15 },
+    ],
+  };
   render() {
     return (
-      <div>
-        <div>
-          <Hero />
-        </div>
-        <div>
-          <Hero />
-        </div>
-        <div>
-          <Hero />
+      <div className="container">
+        <div className="row">
+          {this.state.allAvengers.map((avenger) => (
+            <div key={avenger.id} className="col"><Hero /></div>
+          ))}
         </div>
       </div>
     );
