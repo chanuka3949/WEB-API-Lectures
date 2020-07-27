27cc9bbdb969c375684d3c064b0d02b8b09bc00b
chanuka3949 <chanukasds@outlook.com>
2020-07-27 17:40:51 +0530

    3 Heros

--- Heros-Client/src/components/Heros.jsx
+++ Heros-Client/src/components/Heros.jsx
@@ -2,16 +2,22 @@ import React, { Component } from "react";
 import Hero from "./Hero";
 
 class Heros extends Component {
-    state = {};
-    render() {
-        return (
-            <div>
-            <div><Hero/></div>
-            <div><Hero/></div>
-            <div><Hero/></div>
+  state = {};
+  render() {
+    return (
+      <div>
+        <div>
+          <Hero />
         </div>
-        )
-    }
+        <div>
+          <Hero />
+        </div>
+        <div>
+          <Hero />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Heros;
